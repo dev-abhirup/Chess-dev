96dc3d0049b3b0428524415adb04457f447e2771
Abhirup Mitra <mitraabhirup2002@gmail.com>
2026-07-05 05:19:00 +0530

    Initial commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,30 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './modules/nav-menu/nav-menu.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      RouterTestingModule,
+      HttpClientTestingModule,
+      MatDialogModule,
+      NavMenuComponent
+    ],
+    declarations: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'chess-game'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('chess-game');
+  });
+});
